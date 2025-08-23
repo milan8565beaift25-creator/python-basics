c0a6b70251b2fd81b8aa9b6c5958ffb667873c6d
Milan <milan8565.beaift25@chitkara.edu.in>
2025-08-23 19:34:15 +0530

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,31 @@
+a=(input("enter symbol for cal"))
+b=int(input("first number"))
+c=int(input("second number"))
+if a=='+':
+   print(b+c)
+elif a=='*':
+   print(b*c)
+elif a=='%':
+   print(b%c)
+elif a=='/':
+   print(b/c)
+elif a=='-':
+   print(b-c)
+else:
+   print("invalid") 
+while True:
+   a=(input("enter symbol for cal"))
+   b=int(input("first number"))
+   c=int(input("second number"))
+   if a=='+':
+     print(b+c)
+   elif a=='*':
+     print(b*c)
+   elif a=='%':
+     print(b%c)
+   elif a=='/':
+     print(b/c)
+   elif a=='-':
+      print(b-c)
+   else:
+      print("invalid") 
