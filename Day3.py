4772a8684fc56ee3fa8fb53599fa0636a8a79d93
Milan <milan8565.beaift25@chitkara.edu.in>
2025-08-25 20:05:30 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,20 @@
+#python loops:
+#for loop:
+#for i in range(1,10000):
+    #print("numbers:",i)
+
+
+ 
+#fruits=["cherry","grapes","mango","apple"]
+#print(fruits[0])
+#print(fruits)
+#for fruit in fruits:
+    #print("i like",fruit)
+
+#while loop condition
+#code block
+
+i=1
+while i<=5:
+    print("Number :",i)
+    i+=1
