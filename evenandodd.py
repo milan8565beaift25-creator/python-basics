bd1fbe249810dab7837b12db90e4bff46be860be
Milan <milan8565.beaift25@chitkara.edu.in>
2025-08-26 14:43:42 +0530

    Add files via upload

---
+++ evenandodd.py
@@ -0,0 +1,6 @@
+#write a program to check whether the given number is even or odd using %:
+a=int(input("enter a number:"))
+if a%2==0:
+    print("the given number is even")
+else:
+    print("the given number is odd")
