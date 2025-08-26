96ae72ceaa8d63ff0223c01aa3a235717b4ad1a8
Milan <milan8565.beaift25@chitkara.edu.in>
2025-08-26 14:10:05 +0530

    Add files via upload

---
+++ remandquo.py
@@ -0,0 +1,5 @@
+#write a program to find the remainder and quotient when one number is divided by another:
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+print("remainder:",a%b)
+print("quotient:",a/b)
---
+++ sumanddiff.py
@@ -0,0 +1,9 @@
+#write a program to find the sum, difference,product,division of two numbers entered by the user:
+a=int(input("First Number:"))
+b=int(input("Second Number:"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
