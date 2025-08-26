3acc39249f6918130b0cd217e9a245748de8f05a
Milan <milan8565.beaift25@chitkara.edu.in>
2025-08-26 10:25:44 +0530

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,51 @@
+#print the first 10 natural numbers
+#for i in range(1,11):
+  #print(i)
+
+
+
+#print a multiplication table of a number
+#a=int(input("enter a number:"))
+#for x in range(0,11):
+    #i=a*x
+    #print(i)
+
+
+#print all even numbers from 1 to 100
+#i=2
+#while i<=100:
+        #print("even number",i)
+        #i+=2 
+
+
+#to print reverse numbers from n to 1
+#n=int(input("enter n:"))
+#for i in range(n,0,-1):
+    #print(i)
+
+
+#to get square numbers of all the numbers from 1 to 10
+#n=int(input("enter n:"))
+#for i in range(1,10):
+    #a=i**n
+    #print(a)
+
+
+# while True:
+#     num=int(input("enter a number:"))
+#     fact=1
+#     for i in range(1,num+1):
+#         fact=fact*i
+#     print("the factorial of",num,"is:",fact)
+
+
+#print fibonacci series
+n=int(input("enter number of terms:"))
+a=0
+b=1
+print("fibonacci series:")
+for i in range(n):
+    print(a)
+    a,b=b,a+b
+
+
---
+++ operators.py
@@ -0,0 +1,42 @@
+#airthmetic operators:
+# a=8
+# b=6
+# c=a+b
+# d=a-b
+# e=a/b
+# f=a*b
+# g=a%b
+# h=a**b
+# print("the respective values after operation:",c,d,e,f,g,h)
+
+
+# #assignment operators
+# a=2
+# b=3
+# print(a,b)
+
+# a+=9
+# print("answer:",a)
+
+# b-=4
+# print("answer:",b)
+
+# #logical operators
+# a=67
+# b=90
+# print("the respective answer is:",a>=b and a==b,
+# a<b and a!=b,
+# a>b and a==b,
+# a<=b and a==b,
+# a<b and a==b,
+# a>b and a!=b)
+
+# print("the respective answer is:",a>=b or a==b,
+# a<b or a!=b,
+# a>b or a==b,
+# a<=b or a==b,
+# a<b or a==b,
+# a>b or a!=b)
+
+
+
