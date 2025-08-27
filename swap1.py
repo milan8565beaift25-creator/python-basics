bceda12f02b9abdd9359544d8490ae225d73d65f
Milan <milan8565.beaift25@chitkara.edu.in>
2025-08-27 09:24:18 +0530

    Add files via upload

---
+++ swap1.py
@@ -0,0 +1,8 @@
+#write a program to swap two numbers without using a third variable:
+a=int(input("enter first numer:"))
+b=int(input("enter second number:"))
+print(f"before swaping: a={a},b={b}") #f uses {} these brackets which represents replacement fields so "f" is important to write
+a=a+b
+b=a-b
+a=a-b
+print(f"after swaping: a={a},b={b}")
