70e6102b8064ce869e26d42a5873762e32339636
Milan <milan8565.beaift25@chitkara.edu.in>
2025-08-31 20:11:18 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,188 @@
+# a=int(input("enter some number:"))
+# b=int(input("enter second number:"))
+# c=a+b
+# d=a-b
+# e=a*b
+# f=a//b
+# g=a/b
+# h=a%b
+# print(c,d,e,f,g,h)
+
+# a=int(input("enter marks:"))
+# if a>100:
+#     print("Not Possible")
+# elif a>=90:
+#     print("S")
+# elif a>=75:
+#     print("A")
+# elif a>=60:
+#     print("B")
+# elif a>=45:
+#     print("C")
+# elif a>=33:
+#     print("D")
+# elif 0<a<33:
+#     print("Fail")
+# else:
+#     print("Error")
+  
+
+# a=int(input("enter age:"))
+# if a>18:
+#     print("eligible for vote")
+# elif a==18:
+#     print("wait for some time")
+# elif 0<a<18:
+#     print("not eligible")
+# else:
+#     print("error")
+
+# for i in range(1,100):
+#     print(i)
+
+# fruits=("cherry","mango","apple","grapes")
+# print(fruits[3])
+# print(fruits)
+# for fruit in fruits:
+#     print("i like:",fruit)
+
+
+# for i in range(1,11):
+#     print(i)
+
+# n=int(input("enter number:"))
+# for i in range(1,100):
+#     print(n*i)
+
+
+# i=2
+# while i<=100:
+#     print("table",i)
+#     i+=2
+
+# a=int(input("enter number:"))
+# num=len(str(a))
+# print("count:",num)
+
+
+# for i in range(1,11):
+#     print("square",i**2)
+
+
+# n=int(input("enter number:"))
+# a=0
+# b=1
+# print("fibonacci series:")
+# for i in range(n):
+#     print(a)
+#     a,b=b,b+a
+
+
+# num=int(input("enter number:"))
+# fact=1
+# for i in range(1,num+1):
+#     fact=fact*i
+# print('factorial',fact) 
+
+
+# n=int(input("enter number:"))
+# if n<0:
+#     print("invalid")
+# else:
+#     print(n*(n+1)//2)
+ 
+# n=int(input("enter number:"))
+# for i in range(n,0,-1):
+#     print("reverse",i)
+    
+
+# a=int(input("enter first number:"))
+# b=int(input("enter second number:"))
+# c="sum",a+b
+# d="multiply",a*b
+# e="subtraction",a-b
+# f="factorise",a//b
+# g="quotient",a/b
+# h="remainder",a%b
+# print(c,d,e,f,g,h)
+
+
+
+# n=int(input("enter the number:"))
+# if n%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# a=int(input("enter first number:"))
+# b=int(input("enter second number:"))
+# print(f"before swap:a={a},b={b}")
+# a=a+b
+# b=a-b
+# a=a-b
+# print(f"after swapa={a},b={b}")
+
+
+# a=int(input("enter first number:"))
+# b=int(input("enter second number:"))
+# print(f"before swap:a={a},b={b}")
+# print(f"after swapa={b},b={a}")
+
+# n=int(input("enter number:"))
+# if n%5==0 and n>100:
+#     print("true")
+# else:
+#     print("false")
+
+# while True:
+#    a=(input("enter symbol for cal"))
+#    b=int(input("first number"))
+#    c=int(input("second number"))
+#    if a=='+':
+#      print(b+c)
+#    elif a=='*':
+#      print(b*c)
+#    elif a=='%':
+#      print(b%c)
+#    elif a=='/':
+#      print(b/c)
+#    elif a=='-':
+#       print(b-c)
+#    else:
+#       print("invalid") 
+
+# for i in range(1,5):
+#     for j in range(1,7):
+#         print(i*j)
+#     print()
+
+# n=int(input("enter number:"))
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+# n=int(input("enter number:"))
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+# n=int(input("enter number:"))
+# a=1
+# b=2
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(a,end=" ")
+#         a,b=b,b+1
+#     print()
+
+
+# n=int(input("enter number:"))
+# a=1
+# b=2
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(a,end=" ")
+#     a,b=b,b+1
+#     print()
