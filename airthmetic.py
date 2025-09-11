94c9357b8a818a8aa128718c62e11e4ed6dba5ea
Milan <milan8565.beaift25@chitkara.edu.in>
2025-09-11 10:38:31 +0530

    Add files via upload

---
+++ airthmetic.py
@@ -0,0 +1,4 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+print("sum:",a+b,"difference:",a-b,
+    "multiply:",a*b,"division:",a/b,"quotient:",a%b,"remainder:",a//b,"power:",a**b)
---
+++ swap2.py
@@ -0,0 +1,4 @@
+a=int(input("enter first numer:"))
+b=int(input("enter second number:"))
+print("before swaping: a = ",a,"b = ",b) 
+print("after swaping: a = ",b,"b = ",a)
